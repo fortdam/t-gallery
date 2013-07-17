fa02cad74353559a813284eeb28d60df8cfa80ab
dltang <zhiming.tang@tcl.com>
2013-07-17 18:55:21 +0800

    display real album

--- t-gallery/src/com/example/t_gallery/GalleryList.java
+++ t-gallery/src/com/example/t_gallery/GalleryList.java
@@ -1,19 +1,20 @@
 package com.example.t_gallery;
 
-import java.io.File;
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore.Images.Media;
 import android.provider.MediaStore.Images.Thumbnails;
-import android.app.Activity;
+import android.animation.ObjectAnimator;
 import android.app.ExpandableListActivity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.DataSetObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.LruCache;
@@ -23,289 +24,84 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-class GalleryListAdapter extends BaseExpandableListAdapter{
 
-	
-	GalleryListAdapter(Context aContext){
-		context = aContext;
-	}
-	
-	static class ViewHolder{
-		ImageView icons[] = new ImageView[4];
-		BitmapWorkerTask task[] = new BitmapWorkerTask[4];
-	}
-	
-	class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap>{
-
-		private final WeakReference<ImageView> iconReference;
-		private int index;
-		
-		public BitmapWorkerTask(ImageView icon){
-			iconReference = new WeakReference<ImageView>(icon);
-		}
-		
-		@Override
-		protected  Bitmap doInBackground(Integer... params) {
-			// TODO Auto-generated method stub
-			GalleryList app = (GalleryList)context;
-			long id = 0;
-			index = params[0];
-		    BitmapFactory.Options options = new BitmapFactory.Options();	
-
-			
-			synchronized (app.mImageList){
-			app.mImageList.moveToPosition(params[0]);
-			id = app.mImageList.getLong(app.mImageList.getColumnIndex(Media._ID));
-			}
-			
-			Bitmap thumb = Thumbnails.getThumbnail(app.getContentResolver(), id, Thumbnails.MINI_KIND, options);
-			int height = thumb.getHeight();
-			int width = thumb.getWidth();
-			
-			if (height > width){
-				thumb = Bitmap.createBitmap(thumb, 0, (height-width)/2, width, width);
-			}
-			else{
-				thumb = Bitmap.createBitmap(thumb, (width-height)/2, 0, height, height);
-			}
-			
-	        return Bitmap.createScaledBitmap(thumb,170,170,false);
-	
-			/*BitmapFactory.Options option = new BitmapFactory.Options();
-			option.inJustDecodeBounds = true;
-			BitmapFactory.decodeFile(app.cameraFiles.get(params[0]), option);
-			
-			float xRatio = (float)option.outWidth / (float)270;
-			float yRatio = (float)option.outHeight / (float)270;
-			
-			float scaleRatio = (xRatio<yRatio)?xRatio:yRatio;
-			
-			option.inJustDecodeBounds = false;
-			option.inSampleSize = Math.round(scaleRatio);
-			index = params[0];
-			return BitmapFactory.decodeFile(app.cameraFiles.get(params[0]), option);*/
-		}
-		
-		public void onPostExecute(Bitmap bitmap){
-			GalleryList app = (GalleryList)context;
-			
-			if (iconReference != null && bitmap != null){
-				final ImageView iconView = iconReference.get();
-				
-				if (iconView != null){
-					app.addBitmapToRamCache(index, bitmap);
-					iconView.setImageBitmap(bitmap);
-				}
-			}
-		}
-		
-	}
-	
-	private Context context;
-
-	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return groupPosition*100000 + childPosition;
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return groupPosition*100000 + childPosition;
-	}
-
-	@Override
-	public View getChildView(int groupPosition, int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		LinearLayout line;
-		GalleryList app = (GalleryList)context;
-		
-		Log.e("t-Gallery","Fetch child view of "+groupPosition+"/"+childPosition);
-		
-		ViewHolder holder;
-		
-		if (convertView == null){
-			line = new LinearLayout(context);
-			line.setOrientation(LinearLayout.HORIZONTAL);
-			
-            holder = new ViewHolder();
-            
-            for (int i=0; i<4; i++){
-            	holder.icons[i] = new ImageView(context);
-            	holder.icons[i].setAdjustViewBounds(true);
-				holder.icons[i].setScaleType(ImageView.ScaleType.CENTER_CROP);	
-    			line.addView(holder.icons[i], new LinearLayout.LayoutParams(270,270));
-            }
-			
-			line.setTag(holder);
-		}
-		else {
-			line = (LinearLayout)convertView;
-			holder = (ViewHolder)(line.getTag()); 
-			
-			for (int i=0; i<4; i++){
-				if (holder.task[i] != null){
-					holder.task[i].cancel(true);
-					holder.task[i] = null;
-				}
-			}
-		}
-		
-		if (groupPosition == 0){
-			for (int i=0; i<4; i++){
-				Bitmap bmp = app.getBitmapFromRamCace(childPosition*4 + i);
-				
-				if (bmp != null){
-				    holder.icons[i].setImageBitmap(bmp);	
-				}
-				else{
-				    BitmapWorkerTask task = new BitmapWorkerTask(holder.icons[i]);
-				    task.execute(childPosition*4 + i);
-				    holder.task[i] = task;
-				    holder.icons[i].setImageResource(R.drawable.empty_photo);
-				}			
-			}
-		}
-		else {
-		    int offset = childPosition%6;
-
-		    for (int i=0; i<4; i++){
-			    holder.icons[i].setImageResource(R.drawable.photo01 + offset*4 + i);
-		    }
-		}
-		return line;
-	}
-
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		// TODO Auto-generated method stub
-		if (groupPosition == 0){
-			GalleryList app = (GalleryList)context;
-			return (app.mImageList.getCount()/4);
-		}
-		else if (groupPosition == 1){
-			return 4;
-		}
-		else 
-		{
-		    return 10000;
-		}
-	}
-
-	@Override
-	public Object getGroup(int groupPosition) {
-		// TODO Auto-generated method stub
-		return groupPosition;
-	}
-
-	@Override
-	public int getGroupCount() {
-		// TODO Auto-generated method stub
-		return 10;
-	}
-
-	@Override
-	public long getGroupId(int groupPosition) {
-		// TODO Auto-generated method stub
-		return groupPosition;
-	}
-
-	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		TextView text;
-		
-		if (convertView == null){
-			text = new TextView(context);
-		}
-		else{
-			text = (TextView)convertView;
-		}
-		
-		if (groupPosition == 0){
-			text.setText(new String("    Camera"));
-		}
-		else if (groupPosition == 1){
-			text.setText(new String("    Small Gallery"));
-		}
-		else {
-		    text.setText(new String("    Big Gallery "));
-		}
-		
-		text.setTextSize(30);
-		
-		return text;
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-
-}
 
 public class GalleryList extends ExpandableListActivity 
 	implements OnScrollListener{
 	
-	public ArrayList<String> cameraFiles = new ArrayList<String>();
 	
-	private LruCache<Integer, Bitmap> mRamCache;
+	static public class Config{
+		/*Application configuration*/
+		static public final int MAX_ALBUM = 1000;
+		static public final int MAX_PICTURE_IN_ALBUM = 10000;
+		
+		static public final int THUMBNAILS_PER_LINE = 4;
+		static public final int THUMBNAIL_WIDTH = 270;
+		static public final int THUMBNAIL_HEIGHT = 270;
+		
+		/*RAM cache*/
+		static public final int RAM_CACHE_SIZE_KB = (int)(Runtime.getRuntime().maxMemory()/4096);
+		
+		/*Content query*/
+		static public final Uri MEDIA_URI = Media.EXTERNAL_CONTENT_URI;
+		static public final String ALBUM_PROJECTION[] = {Media.BUCKET_ID, Media.BUCKET_DISPLAY_NAME};
+		static public final String ALBUM_WHERE_CLAUSE = "1) GROUP BY (1"; /*This is a trick to use GROUP BY */
+		static public final String IMAGE_PROJECTION[] = {Media._ID};
+		static public final String IMAGE_WHERE_CLAUSE = Media.BUCKET_ID + " = ?";
+	}
+	
+	private void fetchGalleryList(){
+		
+		mGalleryList = getContentResolver().query(
+				Config.MEDIA_URI, 
+				Config.ALBUM_PROJECTION, 
+				Config.ALBUM_WHERE_CLAUSE, 
+				null, null);
+	
+		mImageLists = new Cursor[mGalleryList.getCount()];
+		
+		for (int i=0; i<mGalleryList.getCount(); i++){
+			mGalleryList.moveToPosition(i);
+			String galleryId = mGalleryList.getString(mGalleryList.getColumnIndex(Media.BUCKET_ID));
+			mImageLists[i] = getContentResolver().query(
+					Config.MEDIA_URI, 
+					Config.IMAGE_PROJECTION, 
+					Config.IMAGE_WHERE_CLAUSE, 
+					new String[]{mGalleryList.getString(mGalleryList.getColumnIndex(Media.BUCKET_ID))}, 
+					null);
+		}
+	}
 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_gallery_list);
-				
-		mImageList = getContentResolver().query(Media.EXTERNAL_CONTENT_URI, new String[]{Media._ID}, null, null, null);
+		setContentView(R.layout.activity_gallery_list);
 		
-		final int maxMemory = (int)(Runtime.getRuntime().maxMemory() / 1024);
-		final int cacheSize = maxMemory / 4;
+		fetchGalleryList();						
 		
-		mRamCache = new LruCache<Integer, Bitmap>(cacheSize){
-			protected int sizeOf(Integer key, Bitmap bmp){
+		mRamCache = new LruCache<Long, Bitmap>(Config.RAM_CACHE_SIZE_KB){
+			protected int sizeOf(Long key, Bitmap bmp){
 				return (bmp.getByteCount()/1024);
 			}
 		};
-		
-		/*
-		File cameraDir = new File(new String("/sdcard/DCIM/Camera"));
-		String fileNames[] = cameraDir.list();
-		String suffix = new String("jpg");
-		
-		for(int i=0; i<fileNames.length; i++){
-			if (fileNames[i].endsWith(suffix)){
-				String prefix = new String("/sdcard/DCIM/Camera/");
-				cameraFiles.add(prefix.concat(fileNames[i]));
-			}
-		}
-		 */
-		
 	
-		setListAdapter(new GalleryListAdapter(this));
+		setListAdapter(new GalleryListAdapter());
 		getExpandableListView().setOnScrollListener(this);
+	}
+	
+	private TextView shortcut;
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		shortcut = (TextView)findViewById(R.id.collapse_shortcut);		
 	}
 
 	@Override
@@ -315,35 +111,301 @@ public class GalleryList extends ExpandableListActivity
 		return true;
 	}
 		
-	public synchronized Bitmap getBitmapFromRamCace(int key){
+	private synchronized Bitmap getBitmapFromRamCace(long key){
 		return mRamCache.get(key);	
 	}
 	
-	public synchronized void addBitmapToRamCache(int key, Bitmap bmp){
+	private synchronized void addBitmapToRamCache(long key, Bitmap bmp){
 		if (null == getBitmapFromRamCace(key)){
 			mRamCache.put(key, bmp);
 		}
 	}
 	
-	public Cursor mImageList;
+	private Cursor mGalleryList;
+	private Cursor mImageLists[];
+	private LruCache<Long, Bitmap> mRamCache;
+	
+	private ObjectAnimator ongoingAnim;
+	private boolean shortcutDisp = true;
+	
+	private void ShowShortcut(){
+		
+		if (shortcutDisp){
+			return;
+		}
+		
+		int height = shortcut.getHeight();
+		ongoingAnim.cancel();
+		
+		ObjectAnimator anim = ObjectAnimator.ofFloat(shortcut, "Y", 0).setDuration(500);
+		anim.start();
+		ongoingAnim = anim;
+		shortcutDisp = true;
+		
+		
+	}
+	
+	private void HideShortcut(boolean immediate){
+		
+		if(!shortcutDisp){
+			return;
+		}
+		
+		int height = shortcut.getHeight();
+		
+		if (ongoingAnim != null)
+		ongoingAnim.cancel();
+		
+		if (immediate){
+			shortcut.setY(0 - height);
+		}
+		else{
+			ObjectAnimator anim = ObjectAnimator.ofFloat(shortcut, "Y", 0-height).setDuration(500);
+			anim.start();
+			ongoingAnim = anim;
+		}
+		shortcutDisp = false;
+	}
+	
+	private int scrollState = AbsListView.OnScrollListener.SCROLL_STATE_IDLE;
 
-
+	private int prevFirstItem = -1;
+	private int prevVisibleItemCount = -1;
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem,
 			int visibleItemCount, int totalItemCount) {
-		// TODO Auto-generated method stub
-		
-		/*ExpandableListView list = getExpandableListView();
-		long index = list.getExpandableListPosition(firstVisibleItem);
-		int groupIndex = list.getPackedPositionGroup(index);
-		int childIndex = list.getPackedPositionChild(index);onChild(index);
-		Log.i("t-Gallery", "on scrolling "+index+childIndex+groupIndex);*/
+
+		if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+			if (prevFirstItem == -1){
+				prevFirstItem = firstVisibleItem;
+				prevVisibleItemCount = visibleItemCount;
+			}
+			else {
+				if (prevFirstItem >  firstVisibleItem){
+					ShowShortcut();
+				}
+				else if (prevFirstItem < firstVisibleItem){
+					HideShortcut(false);
+				}
+			}
+		}
+		else if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_FLING){
+			prevFirstItem = -1;
+			ExpandableListView list = getExpandableListView();
+			long index = list.getExpandableListPosition(firstVisibleItem);
+			int groupIndex = list.getPackedPositionGroup(index);
+			int childIndex = list.getPackedPositionChild(index);
+			
+			if (childIndex == -1){
+				HideShortcut(false);
+			}
+		}
 	}
 
+	private Timer timerToHide;
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		// TODO Auto-generated method stub
 		
+		if (this.scrollState == AbsListView.OnScrollListener.SCROLL_STATE_FLING 
+				&& scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE){
+			if (shortcutDisp){
+				timerToHide = new Timer();
+				timerToHide.schedule(new TimerTask(){
+					
+					Handler msgHandler = new Handler(){
+						public void handleMessage(Message msg){
+							if (shortcutDisp){
+								HideShortcut(false);
+							}
+						}
+					};
+
+					@Override
+					public void run() {
+						msgHandler.obtainMessage().sendToTarget();
+					}
+					
+				}, 1500);
+			}
+		}
+		
+		this.scrollState = scrollState;
 	}
 
+	
+	class GalleryListAdapter extends BaseExpandableListAdapter{
+		
+		class BitmapWorkerTask extends AsyncTask<Long, Void, Bitmap>{
+
+			private final WeakReference<ImageView> iconReference;
+			private long id;
+			
+			public BitmapWorkerTask(ImageView icon){
+				iconReference = new WeakReference<ImageView>(icon);
+			}
+			
+			@Override
+			protected  Bitmap doInBackground(Long... params) {
+			    BitmapFactory.Options options = new BitmapFactory.Options();
+			    
+			    id = params[0];
+				
+				Bitmap thumb = Thumbnails.getThumbnail(getContentResolver(), id, Thumbnails.MINI_KIND, options);
+				int height = thumb.getHeight();
+				int width = thumb.getWidth();
+				
+				if (height > width){
+					thumb = Bitmap.createBitmap(thumb, 0, (height-width)/2, width, width);
+				}
+				else{
+					thumb = Bitmap.createBitmap(thumb, (width-height)/2, 0, height, height);
+				}
+				
+		        return Bitmap.createScaledBitmap(thumb,Config.THUMBNAIL_WIDTH,Config.THUMBNAIL_HEIGHT,false);
+		
+			}
+			
+			public void onPostExecute(Bitmap bitmap){
+				
+				if (iconReference != null && bitmap != null){
+					final ImageView iconView = iconReference.get();
+					
+					if (iconView != null){
+						addBitmapToRamCache(id, bitmap);
+						iconView.setImageBitmap(bitmap);
+					}
+				}
+			}
+			
+		}
+		
+	    class ViewHolder{
+			ImageView icons[] = new ImageView[Config.THUMBNAILS_PER_LINE];
+			BitmapWorkerTask task[] = new BitmapWorkerTask[Config.THUMBNAILS_PER_LINE];
+		}
+	    
+		@Override
+		public Object getChild(int groupPosition, int childPosition) {
+			// TODO Auto-generated method stub
+			return groupPosition*Config.MAX_PICTURE_IN_ALBUM + childPosition;
+		}
+
+		@Override
+		public long getChildId(int groupPosition, int childPosition) {
+			// TODO Auto-generated method stub
+			return groupPosition*Config.MAX_PICTURE_IN_ALBUM + childPosition;
+		}
+
+		@Override
+		public View getChildView(int groupPosition, int childPosition,
+				boolean isLastChild, View convertView, ViewGroup parent) {
+			LinearLayout line;			
+			ViewHolder holder;
+			
+			/*Prepare the view (no content yet)*/
+			if (convertView == null){
+				line = new LinearLayout(getApplicationContext());
+				line.setOrientation(LinearLayout.HORIZONTAL);
+				
+	            holder = new ViewHolder();
+	            
+	            for (int i=0; i<Config.THUMBNAILS_PER_LINE; i++){
+	            	holder.icons[i] = new ImageView(getApplicationContext());
+	            	holder.icons[i].setAdjustViewBounds(true);
+					holder.icons[i].setScaleType(ImageView.ScaleType.CENTER_CROP);	
+	    			line.addView(holder.icons[i], new LinearLayout.LayoutParams(Config.THUMBNAIL_WIDTH,Config.THUMBNAIL_HEIGHT));
+	            }
+				
+				line.setTag(holder);
+			}
+			else {
+				line = (LinearLayout)convertView;
+				holder = (ViewHolder)(line.getTag()); 
+				
+				for (int i=0; i<Config.THUMBNAILS_PER_LINE; i++){
+					if (holder.task[i] != null){
+						holder.task[i].cancel(true);
+						holder.task[i] = null;
+					}
+				}
+			}
+			
+			/*Fill in the content*/
+	        for (int i=0; i<Config.THUMBNAILS_PER_LINE; i++){
+	        	
+	        	Cursor imageList = mImageLists[groupPosition];
+	        	
+	        	if (imageList.getCount() <= (childPosition*Config.THUMBNAILS_PER_LINE+i)){
+	        		holder.icons[i].setImageResource(R.drawable.black);
+	        	}
+	        	else {
+	        		imageList.moveToPosition(childPosition*Config.THUMBNAILS_PER_LINE+i);
+	        		long id = imageList.getLong (imageList.getColumnIndex(Media._ID));
+	        		Bitmap bmp = getBitmapFromRamCace(id);
+	        		
+	        		if (bmp != null){
+	        			holder.icons[i].setImageBitmap(bmp);
+	        		}
+	        		else {
+	        			BitmapWorkerTask task = new BitmapWorkerTask(holder.icons[i]);
+					    task.execute(id);
+					    holder.task[i] = task;
+					    holder.icons[i].setImageResource(R.drawable.empty_photo);
+	        		}
+	        	}	
+			}
+
+			return line;
+		}
+
+		@Override
+		public int getChildrenCount(int groupPosition) {
+			return (mImageLists[groupPosition].getCount() + Config.THUMBNAILS_PER_LINE - 1)/Config.THUMBNAILS_PER_LINE;
+		}
+
+		@Override
+		public Object getGroup(int groupPosition) {
+			return groupPosition;
+		}
+
+		@Override
+		public int getGroupCount() {
+			return mImageLists.length;
+		}
+
+		@Override
+		public long getGroupId(int groupPosition) {
+			return groupPosition;
+		}
+
+		@Override
+		public View getGroupView(int groupPosition, boolean isExpanded,
+				View convertView, ViewGroup parent) {
+			
+			TextView text;
+			
+			if (convertView == null){
+				text = new TextView(getApplicationContext());
+			}
+			else {
+				text = (TextView)convertView;
+			}
+			
+			mGalleryList.moveToPosition(groupPosition);
+			text.setText("     "+mGalleryList.getString(mGalleryList.getColumnIndex(Media.BUCKET_DISPLAY_NAME)));
+			text.setTextSize(30);
+			
+			return text;
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		@Override
+		public boolean isChildSelectable(int groupPosition, int childPosition) {
+			return false;
+		}
+	}
 }
